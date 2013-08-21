623765b1a60aef55fdb8f7e0ef3552e64e1cefdb
kimyee <kimyee.16@gmail.com>
2013-08-20 22:37:53 -0400

    second commit

--- RubyFundamentals.rb
+++ RubyFundamentals.rb
@@ -92,3 +92,14 @@ assert_equal roses, "red"
 assert_equal violets, "blue"
 
 Exercise: Hiring Programmers - Boolean Expressions in Ruby
+is_an_experienced_ruby_programmer =
+  (candidate.languages_worked_with.include? 'Ruby') &&
+  (candidate.years_of_experience >= 2 || candidate.github_points >= 500) &&
+  ! (candidate.age < 15 || candidate.applied_recently?)
+
+  Exercise: Ternary Operator
+  a = "Miles O'Brien"
+b = "Barack Obama"
+
+assert_equal ((a =~ /[ ]\w'/) ?"Irish" : "Not Irish"), "Irish"
+assert_equal ((b =~ /[ ]\w'/) ?"Irish" : "Not Irish"), "Not Irish"
